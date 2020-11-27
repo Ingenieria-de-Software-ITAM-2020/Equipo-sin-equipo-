19e0358c145ddcea3f7a80980358fd28409020d0
Reach-AM <alonsomendiolareach@gmail.com>
2020-11-26 20:14:07 -0600

    chat update

--- Chat/assets/js/chat.js
+++ Chat/assets/js/chat.js
@@ -1,6 +1,60 @@
+var firstmsg = true;
+
 function textEnter() {
+  var chat = document.getElementById("chat");
   var txtmsg = document.getElementById("text").value;
-  alert(txtmsg);
+  if(firstmsg) {
+    firstmsg = false;
+
+    var divider = document.createElement("P");
+    divider.innerHTML = "hoy";
+    chat.appendChild(divider);
+
+    var today = document.createElement("DIV");
+    today.style.display= "grid";
+    today.id = "today";
+    chat.appendChild(today);
+    today = document.getElementById("today");
+  }
+  var today = document.getElementById("today");
+  var t = document.createElement("DIV");
+  t.style.position = "relative";
+
+  var d = document.createElement("DIV");
+  d.style.position = "relative";
+
+  var text = document.createElement("P");
+  text.style.display = "inline-block";
+  text.style.maxWidth = "85%";
+  text.style.padding = "10px";
+  text.style.borderRadius = "15px";
+
+  var da = new Date();
+  var currentTime = "";
+  if(da.getHours() < 10) {
+    if(da.getMinutes() < 10) {
+      var currentTime = "0" + da.getHours() + ":0" + da.getMinutes();
+    }
+  	var currentTime = "0" + da.getHours() + ":" + da.getMinutes();
+  } else {
+    var currentTime = da.getHours() + ":" + da.getMinutes();
+  }
+
+  var date = document.createElement("P");
+  date.innerHTML = currentTime;
+  date.style.position = "absolute";
+  date.style.right = "10px";
+  date.style.bottom = "8px";
+  date.style.fontWeight= "bold";
+
+  text.style.backgroundColor = "rgb(80,220,100)";
+  text.style.cssFloat = "right";
+  text.innerHTML = document.getElementById("text").value;
+
+  t.appendChild(text);
+  d.appendChild(date);
+  today.appendChild(t);
+  today.appendChild(d);
 }
 
 
@@ -23,7 +77,17 @@ function LoadFile() {
 
     var msg = rawtxt.split("\n");
 
-    document.getElementById("nombre").innerHTML = msg[0];
+    if(msg[0].charAt(0) === "$") {
+      document.getElementById("nombre").innerHTML = msg[0].substr(1,msg[0].length-2);
+    } else if(msg[0].charAt(0) === "@") {
+      document.getElementById("nombre").innerHTML = msg[0].substr(1,msg[0].length-2);
+      var divider = document.createElement("P");
+      divider.innerHTML = "Bienvenidos al curso de " + msg[0].substr(1,msg[0].length-2);
+      divider.style.fontSize = "1.3em";
+      chat.appendChild(divider);
+    } else {
+      document.getElementById("nombre").innerHTML = msg[0];
+    }
     var day = "00", month = "00", year = "00";
     for (var i = 1; i < msg.length; i++) {
       if(msg[i].charAt(0) === "[") {
@@ -50,7 +114,6 @@ function LoadFile() {
         d.style.position = "relative";
 
         var text = document.createElement("P");
-        text.innerHTML = msg[i].substr(22);
         text.style.display = "inline-block";
         text.style.maxWidth = "85%";
         text.style.padding = "10px";
@@ -66,10 +129,19 @@ function LoadFile() {
         if(msg[i].charAt(19) === "=") {
           text.style.backgroundColor = "rgb(80,220,100)";
           text.style.cssFloat = "right";
-        } else {
+          text.innerHTML = msg[i].split("@$%&amp;")[1];
+        } else if(msg[i].charAt(19) === "+") {
           text.style.backgroundColor = "rgb(1, 103, 81)";
           text.style.color = "white";
           t.style.left = "10px";
+          text.innerHTML = msg[i].split("@$%&amp;")[1];
+        } else {
+          text.style.color = "black";
+          t.style.left = "10px";
+          var ttt = msg[i].split("@$%&amp;");
+          var info = ttt[0].split("%");
+          text.style.backgroundColor = info[2];
+          text.innerHTML = info[1] + "<br>" + ttt[1];
         }
 
         t.appendChild(text);
